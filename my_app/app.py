f39451d778215e8dccd442ce25f5795512fd95d4
code-JOA <joshbillion1@gmail.com>
2020-06-29 14:55:41 +0100

    moved models into models files

--- my_app/app.py
+++ my_app/app.py
@@ -7,6 +7,22 @@ import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use("Agg")
 # import seaborn as sns
+import hashlib
+# passlib , bcrypt
+
+# database
+from my_database import *
+
+# securing password using hashlib
+def generate_hashes(password):
+    return hashlib.sha256(str.encode(password)).hexdigest()
+
+
+# verying password
+def verify_hashes(password):
+    if generate_hashes(password) == hashed_text:
+        return hashed_text
+    return False
 
 
 def main():
@@ -26,7 +42,11 @@ def main():
         username = st.sidebar.text_input("Username")
         password = st.sidebar.text_input("Password" , type="password")
         if st.sidebar.checkbox("Login"):
-            if password == "12345":
+            create_usertable()
+            hashed_pswd = generate_hashes(password)
+            result = login_user(username, verify_hashes(password,hashed_pswd))
+            # if password == "12345":
+            if result:
                 st.success("Welcome {}".format(username))
 
                 activity = st.selectbox("Activity" , submenu)
@@ -43,15 +63,19 @@ def main():
             new_username = st.text_input("User name")
             new_password = st.text_input("Password" , type='password')
 
-            confirm_password = st.text_input("confirm_Password" , type="password")
+            confirm_password = st.text_input("Confirm Password" , type="password")
             if new_password == confirm_password:
                 st.success("Password confirmed")
             else:
                 st.warning("Password not the same")
 
+            # initialize user table and add data and secure password woth hashlib
             if st.button("Submit"):
-
-
+                create_usertable()
+                hashed_new_password = generate_hashes(new_password)
+                add_userdata(new_username , hashed_new_password)
+                st.succes("You have successfully created a new account")
+                st.info("Login To Get Started")
 
 
 
--- my_app/my_database.py
+++ my_app/my_database.py
@@ -5,24 +5,24 @@ cur = conn.cursor()
 
 # creating the usertable
 def create_usertable():
-    cur.execute("Create Table If Not Exists userstable(username TEXT , password TEXT)")
+    cur.execute("Create Table If Not Exists userstable(username TEXT,password TEXT)")
 
 
 # adding the user
 def add_usertable(username , password):
-    cur.execute("Insert Into Username(username , password) Values (?,?)",(username,password))
+    cur.execute("Insert Into usertable(username , password) Values (?,?)",(username,password))
     conn.commit()
 
 
 # adding user login
 def login_user(username,password):
-    cur.execute("Select * from usertable Where username =? And Password = ?" , (username,password))
+    cur.execute("Select * From usertable Where username =? And Password = ?" , (username,password))
     data = cur.fetchall()
     return data
 
 
 # view all users
 def view_all_users():
-    cur.execute("Select * from usertable")
+    cur.execute("Select * From usertable")
     data = cur.fetchall()
     return data
